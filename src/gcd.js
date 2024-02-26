4895c2334f6100f4c63ead16ada0f5f6d1e491ec
Will Williams <wwilliams@indeed.com>
2024-02-26 13:41:02 -0800

    Adds GCD solution 2 (Brute force solution, less efficient

--- src/gcd.js
+++ src/gcd.js
@@ -26,4 +26,45 @@ function gcd1(a, b) {
  * Write a function named gcd2 that implements algorithm 2 here
  */
 
+function gcd2(a, b) {
+  if (a === 0) {
+    return b;
+  }
+
+  if (b === 0) {
+    return a;
+  }
+
+  const divA = [1, a];
+  const divB = [1, b];
+  const common = [];
+
+  for (let i = 2; i < a; i++) {
+    if (a % i === 0) {
+      divA.push(i);
+    }
+  }
+
+  for (let i = 2; i < b; i++) {
+    if (b % i === 0) {
+      divB.push(i);
+    }
+  }
+
+  for (let i = 0; i < divA.length; i++) {
+    if (divB.includes(divA[i])) {
+      common.push(divA[i]);
+    }
+  }
+
+  let largest = Number.NEGATIVE_INFINITY;
+  for (let i = 0; i < common.length; i++) {
+    if (common[i] > largest) {
+      largest = common[i];
+    }
+  }
+
+  return largest;
+}
+
 module.exports = { gcd1, gcd2 };
