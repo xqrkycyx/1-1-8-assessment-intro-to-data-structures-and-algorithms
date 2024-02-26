0184cfc71aa2169ba27d87be47486b61ee736058
Will Williams <wwilliams@indeed.com>
2024-02-26 13:40:08 -0800

    Adds GCD solution 1 developed by Euclid c. 300 BCE

--- src/gcd.js
+++ src/gcd.js
@@ -1,6 +1,26 @@
 /**
  * Write a function named gcd1 that implements algorithm 1 here
  */
+function gcd1(a, b) {
+  // if a is equal to 0 then the GCD of a and b is b, so return b
+  if (a === 0) {
+    return b;
+  }
+
+  // if b is equal to 0 then the GCD of a and b is a, so return a
+  if (b === 0) {
+    return a;
+  }
+
+  while (b !== 0) {
+    // let r be the remainder of dividing a by b
+    const r = a % b;
+    a = b;
+    b = r;
+  }
+
+  return a;
+}
 
 /**
  * Write a function named gcd2 that implements algorithm 2 here
