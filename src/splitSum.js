7525b46535d234221f95d84801e2bff6cd6b3c6d
Will Williams <wwilliams@indeed.com>
2024-02-26 13:52:22 -0800

    Adds splitsum solution 1 (linear)

--- src/splitSum.js
+++ src/splitSum.js
@@ -2,6 +2,27 @@
  * Write a function named splitSum1 that implements algorithm 1 here
  */
 
+function splitSum1(tours) {
+  // find the sum of the whole array
+  const total = tours.reduce((a, c) => a + c, 0); // O(n)
+
+  let preSum = 0;
+  let postSum = total;
+  let smallest = Number.POSITIVE_INFINITY;
+
+  // for loop goes from second element to second-to-last element
+  for (let i = 0; i < tours.length - 1; i++) {
+    preSum = preSum + tours[i];
+    postSum = postSum - tours[i];
+
+    const difference = Math.abs(preSum - postSum);
+    if (difference < smallest) {
+      smallest = difference;
+    }
+  }
+  return smallest;
+}
+
 /**
  * Write a function named splitSum2 that implements algorithm 2 here
  */
